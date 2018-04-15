a613bb9ddeaad13bcd827e3b50b20f0e0de10641
Olu Meduoye <olu.meduoye@tvplayer.com>
2018-04-15 11:24:22 +0100

    reverting api url for testing

--- public/src/constants/api.js
+++ public/src/constants/api.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://money-turns-staging.herokuapp.com/api';
+const apiUrl = 'https://protected-plains-86085.herokuapp.com/api';
 //const apiUrl = 'http://localhost:3000/api';
 
 export default apiUrl;
